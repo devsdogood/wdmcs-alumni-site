e5bc7a01c6b5056d8b27beae543f92eaf9f6f71b
Darin Webb <darinlwebb@gmail.com>
2021-11-05 22:07:49 -0500

    fix npm version

--- wrappers/ImageCollection.tsx
+++ wrappers/ImageCollection.tsx
@@ -8,7 +8,7 @@ const ImageCollection: React.FC<ImageCollectionProps> = ({ entry }) => {
     return (
         <div style={{border: 'red 1px solid'}}>
             {entry.fields.content.map((image) => (
-                <div>image at {`https:${image.fields.file.url}`}</div>
+                <div key={image.fields.file.url}>image at {`https:${image.fields.file.url}`}</div>
             ))}
         </div>
     );
