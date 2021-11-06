9414f6ee61e48df3af17400194cb23ec24267a9d
justinsoberano <jsoberano2002@gmail.com>
2021-11-06 12:14:02 -0500

    added links to nav menu

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -3,9 +3,13 @@ import { INavigationItem } from "../@types/generated/contentful";
 const NavigationMenu: React.FC<{menuItems: INavigationItem[]}> = ({ menuItems }) => (
     <>
         {menuItems.map((item) => (
-            <div key={item.sys.id}>{item.fields.title}</div>
+            <div key={item.sys.id}>
+                <a href={item.fields.page?.fields.slug || item.fields.externalUrl}> {item.fields.title} </a>
+            </div>
         ))}
         <br />
+
+
     </>
 );
 
