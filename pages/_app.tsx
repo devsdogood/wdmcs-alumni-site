bc6afc8e2bf4c48eefc22db9d6d362359c0277bc
justinsoberano <jsoberano2002@gmail.com>
2021-11-06 13:40:27 -0500

    don't use bootstrap js yet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppLayout from '../wrappers/AppWrapper';
+import { useEffect } from 'react';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // useEffect(() => {
+  //     import('bootstrap/dist/js/bootstrap');
+  // }, []);
   return (
     <AppLayout>
       <Component {...pageProps} />
