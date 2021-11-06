976e8ed9d91af7af72ad77877432c21823134f40
justinsoberano <jsoberano2002@gmail.com>
2021-11-06 13:13:21 -0500

    add fancy navbar

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,16 +1,23 @@
 import { INavigationItem } from "../@types/generated/contentful";
 
-const NavigationMenu: React.FC<{menuItems: INavigationItem[]}> = ({ menuItems }) => (
-    <>
-        {menuItems.map((item) => (
-            <div key={item.sys.id}>
-                <a href={item.fields.page?.fields.slug || item.fields.externalUrl}> {item.fields.title} </a>
+const NavigationMenu: React.FC<{ menuItems: INavigationItem[] }> = ({ menuItems }) => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+                WDMCS Alumni
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    {menuItems.map((item) => (
+                        <a className="nav-link" key={item.sys.id} href={item.fields.page?.fields.slug || item.fields.externalUrl}> {item.fields.title} </a>
+                    ))}
+                </div>
             </div>
-        ))}
-        <br />
-
-
-    </>
+        </div>
+    </nav>
 );
 
 export default NavigationMenu;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppLayout from '../wrappers/AppWrapper';
