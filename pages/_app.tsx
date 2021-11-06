8306703fd9a101d8c36a8431d722ef7c305bd6ae
Darin Webb <darinlwebb@gmail.com>
2021-11-06 14:23:53 -0500

    attempt bootstrap js
    Co-authored-by: Cation8 <Cation8@users.noreply.github.com>
    Co-authored-by: SoupBoi1 <SoupBoi1@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
+// import 'bootstrap/dist/js/bootstrap'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppLayout from '../wrappers/AppWrapper';
